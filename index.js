2789079fbbe37a65b38d36e9d4893a8b43a6c7ab
Oscar Lopez <oscarfrom95@hmail.com>
2020-09-08 23:02:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipe = {eggs: 3, flour: '1 cup', chocolate: '2 cups'}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object.[key] = value
+  return object
+}
