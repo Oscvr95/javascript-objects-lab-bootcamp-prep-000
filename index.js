2c6fefe3cfd7ed771b98f5e296d94e972406b719
Oscar Lopez <oscarfrom95@hmail.com>
2020-09-08 23:37:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  var object = new Object();
-  return delete object.key
+  delete object.key
+  return object
 }
